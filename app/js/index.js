2bdf77ae11990784b5d47ecfe4c982e51b0e5a08
Wil Bown <wilbown@gmail.com>
2019-02-20 23:41:29 -0700

    better blocking progress system

--- app/js/index.js
+++ app/js/index.js
@@ -3,10 +3,6 @@ import $ from 'jquery';
 import EmbarkJS from 'Embark/EmbarkJS';
 import NeurealRewards from 'Embark/contracts/NeurealRewards';
 
-const OpenSeaLink = 'https://rinkeby.opensea.io/assets';
-const ipfsApiGateway = 'https://ipfs.infura.io:5001';
-const ipfsLiveGateway = 'https://cloudflare-ipfs.com';
-
 function error (err) {
   $('#div_error').removeClass('w3-hide');
   $('#div_error #text_description').text(err);
@@ -36,20 +32,30 @@ window.addEventListener('load', async () => {
         });
 
         $('#btn-send-01').addClass('w3-button');
-        $('#btn-send-01').click(async function () {
-          try {
-            $('#progress').removeClass('w3-hide');
-            const accounts = await web3.eth.getAccounts();
-            if (accounts.length < 1) throw new Error('No accounts available, please choose an account.');
-            const balance = await web3.eth.getBalance(accounts[0]);
-            // if (balance < 25) throw new Error('No accounts available, please choose an account with enough funding.');
-            const val = web3.utils.toWei('0.001', 'ether');
-            const receipt = await web3.eth.sendTransaction({ from: accounts[0], to: curContract.options.address, value: val });
-            // const id = receipt.events['Transfer'].returnValues.tokenId;
-          } catch (err) { error(err); }
-          $('#progress').addClass('w3-hide');
-        });
+        $('#btn-send-01').on('click', function () { send(25, curContract); });
+        $('#btn-send-02').addClass('w3-button');
+        $('#btn-send-02').on('click', function () { send(100, curContract); });
+        $('#btn-send-03').addClass('w3-button');
+        $('#btn-send-03').on('click', function () { send(1000, curContract); });
       } catch (err) { error(err); }
     });
   });
 });
+
+async function send (usd, curContract) {
+  try {
+    $('#div_error').addClass('w3-hide');
+    $('#modal_progress').addClass('w3-show');
+    const accounts = await web3.eth.getAccounts();
+    if (accounts.length < 1) throw new Error('No accounts available, please choose an account.');
+    console.log(accounts.length);
+    const balance = await web3.eth.getBalance(accounts[0]);
+    console.log(balance);
+    // if (balance < 25) throw new Error('No accounts available, please choose an account with enough funding.');
+    const val = web3.utils.toWei('0.001', 'ether');
+    const receipt = await web3.eth.sendTransaction({ from: accounts[0], to: curContract.options.address, value: val });
+    // const id = receipt.events['Transfer'].returnValues.tokenId;
+    console.log(receipt);
+  } catch (err) { error(err); }
+  $('#modal_progress').removeClass('w3-show');
+}
