5cad5540a301a2d1372e1a58bd2964ce5d81c950
Wil Bown <wilbown@gmail.com>
2019-04-24 02:52:59 -0600

    Update NeurealRewards.sol

--- contracts/NeurealRewards.sol
+++ contracts/NeurealRewards.sol
@@ -15,9 +15,10 @@ contract NeurealRewards is ERC721Full, ERC721MetadataMintable {
 
     /*** State Variables ***/
     // TODO Set this to the Neureal multisig wallet that will take the ETH from the sale
+    // address payable public constant NEUREAL_ETH_WALLET = 0xb659137d98B9904d39B2c21560E6Cf6ea95b4a12;
     address payable public constant NEUREAL_ETH_WALLET = 0x3B2c9752B55eab06A66A6117E5D428835b03169d;
     // TODO Set this to the address of the wallet that has authority to mint new NFTs
-    address public constant MINTING_PROVIDER = 0xf9311383b518Ed6868126353704dD8139f7A30bE;
+    address private constant MINTING_PROVIDER = 0xf9311383b518Ed6868126353704dD8139f7A30bE;
 
     // TODO set these to Mainnet on release https://developer.kyber.network/docs/MainnetEnvGuide/
     // IKyberNetworkProxy public constant KYBER_NETWORK_PROXY = IKyberNetworkProxy(0x818E6FECD516Ecc3849DAf6845e3EC868087B755); // Mainnet
@@ -27,11 +28,11 @@ contract NeurealRewards is ERC721Full, ERC721MetadataMintable {
     IERC20 public constant ETH_TOKEN_ADDRESS = IERC20(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE); // Same for all networks
     
     
-    uint256 public constant MAX_SUPPLY = 7200; // Maximum tokens possible to mint
+    uint256 public constant MAX_SUPPLY = 150; // Maximum tokens possible to mint
     uint256 public constant COST_DAI = 1 * 10**17; // TODO set DAI price of single card here
 
     address private _owner; // Contract creator
-    function owner() external view returns (address) { return _owner; }
+    // function owner() external view returns (address) { return _owner; }
 
     uint256 private _tokenId = 0; // Unique ID of an NFT
     function tokenId() external view returns (uint256) { return _tokenId; }
@@ -42,29 +43,39 @@ contract NeurealRewards is ERC721Full, ERC721MetadataMintable {
     
     /*** Events ***/
     event TokenPurchase(uint256 _tokenId);
+    event TokenWithdraw(IERC20 token, uint256 amount, address sendTo);
+    event EtherWithdraw(uint256 amount, address sendTo);
 
     /* Initializes contract */
-    constructor() ERC721Full("XYZ NFT Series", "XYZT") public {  _owner = msg.sender; addMinter(MINTING_PROVIDER); }
+    // TODO change name before making live
+    constructor() ERC721Full("XYZ NFT Series", "XYZT") public {
+        _owner = msg.sender;
+        addMinter(MINTING_PROVIDER); // Add allocation minter
+        renounceMinter(); // Remove owner as minter for security
+    }
     // constructor(address minter, uint256 mintcap, string memory name, string memory symbol) ERC721Full(name, symbol) public {
-    //     _cap = mintcap;
+    //     _maxSupply = mintcap;
     //     _owner = msg.sender;
     //     addMinter(minter); // Add allocation minter
     // }
 
-    function mintWithTokenURI(address to, string calldata tokenURI) external {
-        require(_tokenId < MAX_SUPPLY); // Contract is finished, everything is minted
-        require(mintWithTokenURI(to, _tokenId, tokenURI), "");
-        _tokenId = _tokenId.add(1);
-    }
     // To mint, call mintWithTokenURI(to, tokenId, tokenURI)
     // to read asset url, call tokenURI(tokenId)
     
     // TODO override tokenURI(tokenId) to combine an ipfs host, stored seperately and changeable, with the unchangeable ipfs url, ie
     // "https://ipfs.infura.io" + "/ipfs/QmcvYFgYKZa4pzSgTYktHKY8m6bSFsVBmYXPTWEMVv9Sne"
-    // TODO make public facing dapp that lets people send ETH or DAI here, how to seperate public and admin?
-    // TODO add this in with reward amount levels that auto mint for public dapp
     // TODO look at Status plugin to see how to put public dapp on Status on phone
+    // TODO add ability to detect DAI and other ERC20 tokens on Kyber that are sent to the contract and allocate NFT to value
+    // https://www.wealdtech.com/articles/ethereum-smart-service-payment-with-tokens/
 
+    // /* Manual minting */
+    // function mintWithTokenURI(address to, string calldata tokenURI) external {
+    //     require(_tokenId < MAX_SUPPLY, ""); // Contract is finished, everything is minted
+    //     require(mintWithTokenURI(to, _tokenId, tokenURI), "");
+    //     _tokenId = _tokenId.add(1);
+    // }
+
+    /* Current ETH/DAI market rate */
     function getExpectedRate() external view returns(uint256 expectedRate, uint256 slippageRate) {
         return KYBER_NETWORK_PROXY.getExpectedRate(ETH_TOKEN_ADDRESS, DAI_TOKEN_ADDRESS, 1 * 10**18);
     }
@@ -74,28 +85,27 @@ contract NeurealRewards is ERC721Full, ERC721MetadataMintable {
         require(_tokenId < MAX_SUPPLY, ""); // Contract is finished, everything is minted
         require(msg.value > 10**14, ""); // Stop dust spamming, contract only calls, etc
         require(msg.data.length == 0, ""); // Prevent calls to invalid functions
-        require(msg.sender != address(0), ""); // Prevent transfer to 0x0 address
+        require(msg.sender != address(0), ""); // Prevent minting to 0x0 address
         require(msg.sender != address(this), ""); // Prevent calls from this.transfer(this)
         // assert(address(this).balance >= msg.value, ""); // this.balance gets updated with msg.value before this function starts
         
-        // TODO change this to denominate in DAI and use exchange to convert
-        uint256 minRate;
+        uint256 minRate; // The trade rate from ETH to DAI, example: 140000000000000000000 (1 ETH = 140 DAI)
         (, minRate) = KYBER_NETWORK_PROXY.getExpectedRate(ETH_TOKEN_ADDRESS, DAI_TOKEN_ADDRESS, msg.value);
-        // will send back tokens to this contract's address
+        // will send back tokens to this contract's address, returns example: 10000000000000000000 (10 DAI)
         uint256 traded = KYBER_NETWORK_PROXY.swapEtherToToken.value(msg.value)(DAI_TOKEN_ADDRESS, minRate);
 
         // check amount
         uint256 tokens = traded.div(COST_DAI);
         require(tokens != 0, ""); // Must purchase at least one item
-        uint256 maxTokenId = _tokenId.add(tokens);
-        require(maxTokenId <= MAX_SUPPLY, ""); // Check if there is enough available to allocate, the whole batch or nothing
+        // uint256 maxTokenId = _tokenId.add(tokens);
+        // require(maxTokenId <= MAX_SUPPLY, ""); // the whole batch or revert, not needed, will not emit any events
 
         address msg_sender = msg.sender;
         for (uint256 i = 0; i < tokens; i++) {
             require(_tokenId < MAX_SUPPLY, ""); // Check if there is enough available to allocate
             _tokenAllocation[_tokenId] = msg_sender;
-            _tokenId = _tokenId.add(1);
             emit TokenPurchase(_tokenId);
+            _tokenId = _tokenId.add(1);
         }
 
         // TODO stop reentrancy
@@ -103,18 +113,33 @@ contract NeurealRewards is ERC721Full, ERC721MetadataMintable {
         // uint256 refund = msg.value.mod(COST_WEI);
     }
 
-    /* Withdraw current available ETH in contract */
-    function withdraw() external {
-        require(msg.sender == _owner); // Only owner
-        uint256 withdrawalValue = address(this).balance;
-        
-        NEUREAL_ETH_WALLET.transfer(withdrawalValue); // This works with our multisig (using 2300 gas stipend)
-        // require(NEUREAL_ETH_WALLET.call.value(withdrawalValue)()); // alternative to be able to send more gas
+    /* Actually mint the allocated token, every token must be allocated before minting */
+    function mintAllocated(uint256 allocatedTokenId, string calldata tokenURI) external {
+        require(allocatedTokenId < _tokenId, ""); // Restrict to currently allocated tokens
+        address to = _tokenAllocation[allocatedTokenId];
+        require(to != address(0), ""); // Prevent minting to 0x0 address
+        require(mintWithTokenURI(to, allocatedTokenId, tokenURI), "");
     }
 
-    function mintAllocated(uint256 allocatedTokenId, string calldata tokenURI) external {
-        address to = _tokenAllocation[allocatedTokenId];
-        require(to != address(0), "");
-        require(mintWithTokenURI(to, allocatedTokenId, tokenURI), "");
+    /* Withdraw all current available ETH in contract */
+    function withdrawEther() external {
+        require(msg.sender == _owner, ""); // Only owner
+        uint256 amount = address(this).balance;
+        require(amount > 0, ""); // Don't call transfer if nothing available
+        NEUREAL_ETH_WALLET.transfer(amount); // This works with our multisig (using 2300 gas stipend)
+        // require(NEUREAL_ETH_WALLET.call.value(amount)(), ""); // alternative to be able to send more gas
+
+        emit EtherWithdraw(amount, NEUREAL_ETH_WALLET);
+    }
+
+    /* Withdraw all current available specified ERC20 token in contract */
+    // TODO send all token to NEUREAL_ETH_WALLET
+    function withdrawToken(IERC20 token) external {
+        require(msg.sender == _owner, ""); // Only owner
+        uint256 amount = token.balanceOf(address(this));
+        require(amount > 0, ""); // Don't call transfer if nothing available
+        require(token.transfer(NEUREAL_ETH_WALLET, amount), "");
+
+        emit TokenWithdraw(token, amount, NEUREAL_ETH_WALLET);
     }
 }
